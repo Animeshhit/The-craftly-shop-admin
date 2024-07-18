38b416081c906eee25d7591c88ef0cea2e292172
Animeshhit <animeshkumbhakarhit2@gmail.com>
2024-07-18 07:51:59 +0530

    home page made btf

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <div className="font-semibold font-poppins text-2xl">DashBoard Will Be Here Soon 💕🚀🚀</div>;
+  return (
+    <>
+      <div className="h-full w-full flex items-center justify-center flex-col">
+        <img className="w-[500px]" src="/design.svg" alt="The Craftly Shop" />
+        <a href="https://thecraftly.shop/" className="font-poppins mt-1">
+          www.thecraftly.shop
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default Home;
